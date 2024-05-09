7fb630e24edeeecaedfc7796607de7c1c9800040
Wesley Hsu <tunahsu45@gmail.com>
2024-05-09 17:15:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,30 +21,38 @@ class VideoProcessor(VideoProcessorBase):
         # Init detection and classification models
         self.detector = DetectHandKeypoint()
         self.classifier = Classifier('models/classification.pt')
+        self.img_pred = None
         
         # Load scaler parameters
         scaler_minmax_path = 'models/scaler_minmax.npy'
         self.min, self.max = np.load(scaler_minmax_path)
         
         self.prediction = None
+        self.counter = 0
+        self.start = time.time()
+        self.fps = 0
 
     def recv(self, frame: av.VideoFrame):
         img = frame.to_ndarray(format='bgr24')
+        self.img_pred = img.copy()
         
         with lock:
+            self.counter += 1
+            self.fps = self.counter / (time.time() - self.start)
+            
             results = self.detector(img)
             
             if results.multi_hand_landmarks:
                 keypoints = self.detector.get_keypoint_list(results.multi_hand_landmarks[0].landmark)
                 scaled_keypoints = scaled_keypoints = np.subtract(keypoints, self.min) / np.subtract(self.max, self.min)
                 self.prediction = self.classifier(scaled_keypoints)
-                img_pred = self.detector.plot(img, results.multi_hand_landmarks[0])
+                self.img_pred = self.detector.plot(self.img_pred, results.multi_hand_landmarks[0])
 
             else:
-                self.prediction = None
-                img_pred = img
+                self.prediction = '❌'
+                self.img_pred = img
                 
-        return av.VideoFrame.from_ndarray(img_pred, format='bgr24')
+        return av.VideoFrame.from_ndarray(img, format='bgr24')
 
 
 def main():
@@ -58,23 +66,27 @@ def main():
         ctx = webrtc_streamer(
             key='stream-detection',
             video_processor_factory=VideoProcessor,
-            media_stream_constraints={
-                'video': {'width': 640},
-                'audio': False
-            },
-            rtc_configuration={
-                # STUN/TURN server is required
-                'iceServers': [{"urls": ["stun:stun.l.google.com:19302"]}]
-            },
+            media_stream_constraints={'video': {'width': 640}, 'audio': False},
+            # STUN/TURN server is required
+            rtc_configuration={'iceServers': [{"urls": ["stun:stun.l.google.com:19302"]}]},
         )
         
     with col2:
-        with st.empty():
-            while ctx.state.playing:
-                with lock:
-                    result = ctx.video_processor.prediction
-                    st.header(f'Result: {result}')
-                    time.sleep(0.001)
+        placeholder = st.empty()
+
+        while ctx.state.playing:
+            with lock:
+                img_pred = ctx.video_processor.img_pred
+                result = ctx.video_processor.prediction
+                fps = ctx.video_processor.fps
+                
+                with placeholder.container():
+                    st.header(f'Result: {result} | FPS: {round(fps, 2)}')
+                    if img_pred is not None:
+                        st.image(img_pred, use_column_width='always', channels='BGR')
+
+                time.sleep(0.001)
+                placeholder.empty()
 
 
 if __name__ == '__main__':
